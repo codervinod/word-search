88afe35307bdd6ba44e3483acd23c8204d7eb186
Vinod Gupta <vinod_gupta@live.com>
2015-10-01 21:32:59 -0700

    Initial Commit

---
+++ word-search/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  word-search
+//
+//  Created by Vinod Gupta on 10/1/15.
+//  Copyright (c) 2015 vinodg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
